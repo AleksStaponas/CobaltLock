be56aa265b9706f6ef3250454576c9526ce63c89
Aleks Staponas <alekssandras.staponas@gmail.com>
2025-07-03 20:58:35 +0100

    Delete src/main/java/GUITester.java
    Unnecessary file deleted

--- src/main/java/GUITester.java
+++
@@ -1,7 +0,0 @@
-public class GUITester {
-    public static void main(String[] args) {
-        GUIDemo gd = new GUIDemo(640,480);
-        gd.setUpGUI();
-        gd.setUpButtonListeners();
-    }
-}
